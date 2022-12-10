889453e3eb86a0eb3bff01bded7e2847352aee34
Elena Gumerova <elenafrontend@ya.ru>
2022-12-10 22:39:31 +0300

    feat(laptop-shop): implement card button

--- js_simple/components/Products/Products.js
+++ js_simple/components/Products/Products.js
@@ -5,22 +5,23 @@ class Products {
     CATALOG.forEach(({ name, img, price }) => {
       htmlCatalogList += `
         <li class="products-item">
-          <h2 class="products-item__title">${name}</h2>
-          <div class="products-item__img">
-            <img class="img" src="${img}" alt="${name} photo">
+          <div class="products-item__wrapper">
+            <h2 class="products-item__title">${name}</h2>
+            <div class="products-item__img">
+              <img class="img" src="${img}" alt="${name} photo">
+            </div>
+            <span class="products-item__price">${price}</span>
+            <button class="products-item__btn button">Добавить в корзину</button>
           </div>
-          <span class="products-item__price">${price}</span>
         </li>
       `
     });
 
-    const htmlCatalog = `
+    ROOT_PRODUCTS.innerHTML = `
       <ul class="products__list list-reset">
         ${htmlCatalogList}
       </ul>
     `;
-
-    ROOT_PRODUCTS.innerHTML = htmlCatalog;
   }
 }
 

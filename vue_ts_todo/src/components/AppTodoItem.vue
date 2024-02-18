cf2ae4f91f33c4e88a313ddf74f9cd5ff70fb6ff
e.gumerova <elenafrontend@ya.ru>
2024-02-18 17:31:33 +0300

    feat(vue3_ts_todos): implement todo item component

---
+++ vue_ts_todo/src/components/AppTodoItem.vue
@@ -0,0 +1,30 @@
+<script lang="ts">
+import {defineComponent, PropType} from 'vue'
+
+export default defineComponent({
+  name: "AppTodoItem",
+
+  props: {
+    todo: {
+      type: Object as PropType<{
+        id: number,
+        text: string
+        done: boolean,
+      }>,
+      required: true,
+    }
+  }
+})
+</script>
+
+<template>
+  <li class="todo-item" :class="{'todo-item--done': todo.done}">
+    <div class="todo-item__status">
+      <i class="bi bi-check2"></i>
+    </div>
+    <span class="todo-item__text">{{ todo.text }}</span>
+    <button class="todo-item__remove-button">
+      <i class="bi bi-trash3"></i>
+    </button>
+  </li>
+</template>
--- vue_ts_todo/src/components/AppTodoList.vue
+++ vue_ts_todo/src/components/AppTodoList.vue
@@ -1,22 +1,20 @@
 <script lang="ts">
 import {defineComponent} from 'vue'
+import AppTodoItem from "@/components/AppTodoItem.vue";
 
 export default defineComponent({
   name: "AppTodoList",
+
+  components: {AppTodoItem},
 })
+
 </script>
 
 <template>
   <ul class="todo-list">
-    <li class="todo-item todo-item--done">
-      <div class="todo-item__status">
-        <i class="bi bi-check2"></i>
-      </div>
-      <span class="todo-item__text">Learn the basics of Vue</span>
-      <button class="todo-item__remove-button">
-        <i class="bi bi-trash3"></i>
-      </button>
-    </li>
+
+    <AppTodoItem :todo="{id: 0, done: true, text: 'Learn the basics of Vue'}" />
+
     <li class="todo-item">
       <div class="todo-item__status">
         <i class="bi bi-check2"></i>
